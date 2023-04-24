8b706f173b6fe84be9c4c7d3e3dff4f7c964691e
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-04-23 23:50:57 -0400

    feat: :art: creamos una función de menú y una estructura con su respectivo puntero dinámico

--- Taller1-DavidHidalgo.cpp
+++ Taller1-DavidHidalgo.cpp
@@ -3,8 +3,36 @@
 #include <string>
 
 using namespace std;
+typedef struct 
+{
+    int array[10];
+    int tamano;
+    string numeroString;
+    int numeroInt;
+    float numeroFloat;
+    bool util;
+} tRegistro;
+typedef tRegistro *tRegistroPtr;
+tRegistroPtr registroPtr = new tRegistro;
 
+
+void menu()
+{
+    bool salir;
+    int opcion;
+    do
+    {
+        cout << "1. Ingresar datos" << endl;
+        cin >> opcion;
+        cout << "2. Mostrar datos" << endl;
+        cin >> opcion;
+        cout << "3. Salir" << endl;
+        salir=true;
+    } while (salir == false);
+}
 int main()
 {
- //texto de prueba
+    menu();
+    delete registroPtr;
+    return 0;
 }
